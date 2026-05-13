f694d3c04752573c0562c365167646af88721a5f
Bodo <b.f.otten@student.tue.nl>
2026-05-13 12:26:13 +0200

    Initial commit
    All files needed + some dummy code I made

---
+++ Registration/registration_project.py
@@ -0,0 +1,10 @@
+"""
+Project code for image registration topics.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import registration as reg
+import registration_util as util
+from IPython.display import display, clear_output
+
